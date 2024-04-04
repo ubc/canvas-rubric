ea16bc69fee490ef1e51bf6fda65580682d02c9d
alisonmyers <alison.myers@ubc.ca>
2024-04-04 11:14:42 -0700

    add back previously commented

--- cli.js
+++ cli.js
@@ -57,7 +57,7 @@ const getAssignmentRubricAndWriteFile = async (courseId, assignmentId, rubricId)
   console.log('Thanks, fetching data now. This may take a bit of time...')
   const rubricData = await getRubric(courseId, assignmentId, rubricId)
   writeToCSV(rubricData, `rubric-${courseId}-${assignmentId}-${rubricId}.csv`)
-  //console.log(`Data is ready now. It's named 'rubric-${courseId}-${assignmentId}-${rubricId}.csv'`)
+  console.log(`Data is ready now. It's named 'rubric-${courseId}-${assignmentId}-${rubricId}.csv'`)
 }
 
 const getQuizRubricAndWriteFile = async (courseId, quizId, rubricId) => {
